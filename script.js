6af17c046a43625091250b87c9f6e567420224af
vradhi108 <84184710+vradhi108@users.noreply.github.com>
2021-07-28 01:22:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+setInterval(() =>{
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotate = 30 * htime + mtime/2;
+    mrotate = 6 * mtime;
+    srotate = 6 * stime;
+    console.log(d);
+    hour.style.transform = `rotate(${hrotate}deg)`;
+    minutes.style.transform = `rotate(${mrotate}deg)`;
+    seconds.style.transform = `rotate(${srotate}deg)`;
+}, 1000);
